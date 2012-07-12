97c946ad9108f85f8da14ddcc43e3691225f6457
Finn Bacall <finn.bacall@cs.man.ac.uk>
2012-07-12 20:19:25 +0100

    Multi user chat now works

---
+++ src/ChatClientThread.java
@@ -0,0 +1,21 @@
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+
+public class ChatClientThread extends Thread {
+	
+	private PrintWriter out;
+	
+	public ChatClientThread(PrintWriter out) {
+		this.out = out;		
+	}
+	
+	public void run() {
+		Scanner stdIn = new Scanner(System.in);
+		String userInput;
+		while((userInput = stdIn.nextLine()) != null) {
+	  	    out.println(userInput); // Send user input to server
+		}
+	}
+
+}
